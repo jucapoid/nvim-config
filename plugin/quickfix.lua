3114cc9e6a237a6417e8323a8ae355c0660a5d95
jucapoid <jucapoid@pm.me>
2026-07-31 12:38:46 +0100

    quickfix support

--- plugin/quickfix.lua
+++ plugin/quickfix.lua
@@ -1,22 +1,38 @@
-local group = vim.api.nvim_create_augroup("Quickfix", {})
+local group = vim.api.nvim_create_augroup("Quickfix", { clear = true })
 
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
 	group = group,
 	pattern = "make",
 	callback = function()
-		local qf = vim.fn.getqflist()
+		local qflist = vim.fn.getqflist()
 
-		if #qf == 0 then
+		if vim.tbl_isempty(qflist) then
 			return
 		end
 
 		vim.cmd("copen")
 
-		for _, item in ipairs(qf) do
+		for _, item in ipairs(qflist) do
 			if item.valid == 1 then
 				vim.cmd("cc")
 				return
 			end
 		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	group = group,
+	pattern = "quickfix",
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+		vim.opt_local.signcolumn = "no"
+		vim.opt_local.wrap = false
+
+		local opts = { buffer = true, silent = true }
+
+		vim.keymap.set("n", "q", "<cmd>cclose<CR>", opts)
+		vim.keymap.set("n", "<CR>", "<CR><C-w>p", opts)
 	end,
 })
